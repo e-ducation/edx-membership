91ba702fb1cc47aa6467fdcc6cb8f372f598ecba
Crews <179455570@qq.com>
2018-12-13 15:47:22 +0800

    fix 自动刷新bug

--- membership/static/membership/js/pay.js
+++ membership/static/membership/js/pay.js
@@ -1,19 +1,4 @@
-window.onload = function(){
-    var time = 3;
-    var odd = function () {
-        if (time <= 0){
-            clearInterval(timer);
-            return ;
-        }
-        time --;
-        $('.wxpay-tips')[0].innerHTML = time + gettext('Return to EliteMBA in _ seconds');
-    }
-    var timer = setInterval(odd,1000)
-    setTimeout(function(){
-            window.location.href = "/vip/card"
-            // console.log('return')
-        },3500)
-}
+
 
 var checkStatus = function () {
     this.start = function(cb,id) {
